a88f2635e6606c69b6ffcbbd52820282c88732c7
Finlay Luxton <luxfi448@student.otago.ac.nz>
2018-05-15 19:26:48 +1200

    -Replaced redundant while loops with for loops -Removed global variable and placed it inside its used search loops to prevent contamination -Updated switch statement to be in correct order -Renamed all search functions with more coherent names

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <stdio.h>
 
+// Replace variable name
 struct S{
     char* firstName;
     char* lastName;
@@ -11,13 +12,11 @@ struct S{
     char* emailAddress;
 };
 
-// This is public visiblity, will be fucked for multiple uses. Place inside of all searches
-static int i;
 static int count;
 
-int ffn(struct S** ss, char* s){
-    //++i doesnt work for these, swap to for loops
-    while(++i < count){
+int findFirstName(struct S** ss, char* s){
+    int i;
+    for(i = 0; i < count; i++){
         // C cannot do this, use the stringcompare method
         // Same for all except phone, since its an int
         if(ss[i]->firstName == s)
@@ -26,24 +25,27 @@ int ffn(struct S** ss, char* s){
     return 0;
 }
 
-int fln(struct S** ss, char* s){
-    while(++i < count){
+int findLastName(struct S** ss, char* s){
+    int i;
+    for(i = 0; i < count; i++){
         if(ss[i]->lastName == s)
             return 1;
     }
     return 0;
 }
 
-int fem(struct S** ss, char* s){
-    while(++i < count){
+int findEmail(struct S** ss, char* s){
+    int i;
+    for(i = 0; i < count; i++){
         if(ss[i]->emailAddress == s)
             return 1;
     }
     return 0;
 }
 
-int fph(struct S** ss, int s){
-    while(++i < count){
+int findPhone(struct S** ss, int s){
+    int i;
+    for(i = 0; i < count; i++){
         if(ss[i]->phone == s)
             return 1;
     }
@@ -55,6 +57,7 @@ int main(int argc, char ** argv) {
     int count = 0;
     (void)argc;
     
+    // 242 code, maybe bst where you str copy in memory allocation so it never overrises itself and thats how you fi the problem good luck me
     struct S** ss = (struct S**) malloc(100*sizeof(ss[0]));
     struct S* s = malloc(sizeof(*s));
 
@@ -79,30 +82,28 @@ int main(int argc, char ** argv) {
     while(command != 0){
         //Should be correct
         char* val = malloc(100*sizeof(val[0]));
-        // 4 lines below should be useless
-
 
         //Command + " " + lookingforinput(eg finn)
         switch(command){
-            // Change order of cases and fix their calls and method names for find methods
+            // Check to see if input is correct
             case 1:
-                printf("looking for email %s\n", val);
-                printf("found it? %d\n", fem(ss, val));
+                printf("Looking for firstname %s\n", val);
+                printf("found it? %d\n", findFirstName(ss, val));
                 break;
             case 2:
-                printf("looking for firstname %s\n", val);
-                printf("found it? %d\n", ffn(ss, val));
+                printf("looking for lastname %s\n", val);
+                printf("found it? %d\n", findLastName(ss, val));
                 break;
             case 3:
-                printf("looking for lastname %s\n", val);
-                printf("found it? %d\n", fln(ss, val));
+                printf("looking for phone %s\n", val);
+                printf("found it? %d\n", findPhone(ss, atoi(val)));
                 break;
             case 4:
                 printf("looking for email %s\n", val);
-                printf("found it? %d\n", fph(ss, atoi(val)));
+                printf("found it? %d\n", findEmail(ss, val));
                 break;
             default:
-                // Add error message
+                fprintf(stderr, "Invalid input. Please input 1, 2, 3, or 4");
                 break;
         }
     }
